dce890f6eac42df50959a32d9a7a663e10396cc6
Laura <laura.heddleston@gmail.com>
2020-03-24 15:02:11 -0700

    Add paths for proxy server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require("path");
+const httpProxy = require("http-proxy");
+const proxy = httpProxy.createProxyServer({});
+
 const PORT = 4000;
 
 /********* Middleware *********/
@@ -9,9 +13,37 @@ app.use(cors());
 
 
 /********* Routes + Controllers *********/
+app.all('/photos', (req, res) => {
+  proxy.web(req, res, {
+    target: "http://localhost:3001/"
+  });
+});
 
+app.all('/recommendations', (req, res) => {
+  proxy.web(req, res, {
+    target: "http://localhost:3009"
+  });
+});
+
+app.all('/api/properties', (req, res) => {
+  proxy.web(req, res, {
+    target: "http://localhost:3000"
+  });
+});
+
+app.all('/api/listing', (req, res) => {
+  proxy.web(req, res, {
+    target: "http://localhost:3500"
+  });
+});
+
+app.all('/api/reviews', (req, res) => {
+  proxy.web(req, res, {
+    target: "http://localhost:3500"
+  });
+});
 
 /********* Start App *********/
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Proxy server listening on port ${PORT}`);
 })
